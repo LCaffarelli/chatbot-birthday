caf0738c7dfe3b4fc30a825d407f895f71fb4d29
clamc068 <laetitia.caffarelli@gmail.com>
2023-05-04 17:31:43 +0200

    ajout d'un message Hello World personnalisé.
    MAJ du readme

--- helloworld.js
+++ helloworld.js
@@ -12,19 +12,32 @@ app.listen(3000, () => {
 });
 
 
-// Afficher Hello World !
+// Afficher Hello World 
 app.post('/api/helloworld', (req, res) => {
     res.send("Hello World POST");
 });
- app.get('/api/helloworld', (req, res) => {
+
+//Pour le navigateur
+app.get('/api/helloworld', (req, res) => {
     res.send("Hello World GET");
 });
 
 
 // Afficher hello world de manière personnalisée
-app.post('/api/hello', (req,res)=>{
-    console.log(`helloWorld`)
-    res.send(req.body)
+app.post('/api/hello', (req, res) => {
+    //création des constantes pour récuperer les éléments du message
+    const { text } = req.body.message;
+    const { displayName } = req.body.message.sender;
+    const { espace } = req.body.space;
+
+    // Permet de renvoyer directement du JSon dans le content-type
+    res.json({ texte: text + espace + displayName })
+
+    //    Autre solution, mais  ne change pas le content type en JSON, il reste en texte/HTML
+    //    res.send(JSON.stringify(text+espace+displayName))
+
+
+
 })
 
 
